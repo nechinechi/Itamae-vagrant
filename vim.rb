ef81718bfb51adb30eff6d5549ba7fc4c7315b1c
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-01-30 18:35:57 +0900

    [add] vim.rb

---
+++ vim.rb
@@ -0,0 +1,7 @@
+execute 'git clone https://github.com/nechinechi/.vim.git'
+# git 'clone .vim' do
+#   destination '~/'
+#   repository 'git@github.com:nechinechi/.vim.git'
+#   revision 'HEAD'
+#   only_if 'test -e .vim'
+# end
