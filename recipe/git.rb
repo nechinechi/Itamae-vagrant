132a98f4a5d070404096375f694eca62864a7498
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-03-03 08:04:06 +0900

    [modify] .gitignore

---
+++ recipe/git.rb
@@ -0,0 +1,7 @@
+package 'git'
+
+# 設定
+execute %(git config --global user.name "#{node[:git][:username]}")
+execute %(git config --global user.email "#{node[:git][:e_mail]}")
+execute 'git config --global core.editor vim'
+
