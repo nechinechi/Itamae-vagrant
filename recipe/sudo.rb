8ccbe0220a66759cb4749860cf2f12791cfe603e
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-03-08 03:48:19 +0900

    modify-ing edit action of file resource

---
+++ recipe/sudo.rb
@@ -0,0 +1,8 @@
+# file "/etc/sudoers" do
+#   action :edit
+#   block do |content|
+#     # content.gsub!("Defaults>-env_reset", "# Defaults>-env_reset")
+#     content.gsub!(/^Defaults>-env_reset/, "# \&")
+#   end
+# end
+
