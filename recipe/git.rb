1448fb2ece843c3b1d30022e6a867b4ef4c523ae
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-05-01 05:22:26 +0900

    [modify] never exec unnecessary command on git.vim

--- recipe/git.rb
+++ recipe/git.rb
@@ -1,8 +1,22 @@
 package 'git'
 
 # 設定
-execute %(git config --global user.name "#{node[:git][:username]}")
-execute %(git config --global user.email "#{node[:git][:e_mail]}")
-execute 'git config --global core.editor vim'
-execute 'git config --global push.default current'
+confirm = '# done provisioning with Itamae'
+[
+  %(user.name "#{node[:git][:username]}"),
+  %(user.email "#{node[:git][:e_mail]}"),
+  "core.editor vim",
+  "push.default current"
+].each do |setting|
+  # execute %(git config --global user.name "#{node[:git][:username]}")
+  # execute %(git config --global user.email "#{node[:git][:e_mail]}")
+  # execute 'git config --global core.editor vim'
+  # execute 'git config --global push.default current'
+  execute "git config --global #{setting}" do
+    not_if "grep '#{confirm}' .gitconfig"
+  end
+end
 
+execute "echo '#{confirm}' >> .gitconfig" do
+  not_if "grep '#{confirm}' .gitconfig"
+end
