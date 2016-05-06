173a3cb06ccea1b1733acfa71328fe2ce51de5a4
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-05-06 13:19:07 +0900

    [add] install rails and nokgiri, bundle

--- recipe/ruby/rubygems.rb
+++ recipe/ruby/rubygems.rb
@@ -1,4 +1,40 @@
-package 'gem' do
-  options '--force-yes'
-  not_if 'which gem'
+# package 'gem' do
+#   options '--force-yes'
+#   not_if 'which gem'
+# end
+
+# package 'gcc-c++'
+
+# %w(libxml2 libxslt libxml2-devel libxslt-devel).each do |pkg|
+#   package pkg
+# end
+
+%w(libxml2-dev libxslt-dev).each do |pkg|
+  package pkg
+end
+
+%w(build-essential patch ruby-dev zlib1g-dev liblzma-dev).each do |pkg|
+  package pkg
+end
+
+%w(libmysqld-dev libssl-dev libffi-dev).each do |pkg|
+  package pkg
+end
+
+%w(bison openssl curl zlib1g libyaml-dev nodejs).each do |pkg|
+  package pkg
+end
+
+# %w(rails bundler).each do |gem|
+#   gem_package gem do
+#     action :install
+#   end
+# end
+
+# execute "su - -c 'gem update --system'"
+
+%w(nokogiri rails bundler).each do |gem|
+  execute "su - -c 'gem install #{gem}'" do
+    not_if "su - -c '#{gem} -v'"
+  end
 end
